22e107618d2d5b1224678bdc4ed570bc5f01b7cb
Hyeokjune Jeon <fantajeon@naver.com>
2017-10-08 04:04:06 +0900

    main.js

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+import xdLocalStorage from "./xdLocalStorage.js";
+import xdLocalStorageEmbedded from "./xdLocalStoragePostMessageApi.js";
+
+
+export default {
+  xdLocalStorage: xdLocalStorage,
+  xdLocalStorageEmbedded: xdLocalStorageEmbedded,
+}
